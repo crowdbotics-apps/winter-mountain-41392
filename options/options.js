87b5289443dcee8832f37a1575ca209e86445e36
Crowdbotics <team@crowdbotics.com>
2023-06-09 17:23:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "winter-mountain-41392",
+  url: "https://winter-mountain-41392.botics.co",
+  api: "https://winter-mountain-41392.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
